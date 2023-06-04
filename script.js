6186fb94c783eec8ec4f11c5419703ca13647e48
elenal63 <91587602+elenal63@users.noreply.github.com>
2023-06-03 21:55:57 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,133 @@
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton = document.getElementById("total-amount-button");
+const productTitle = document.getElementById("product-title");
+const amount = document.getElementById("amount");
+const expenditureValue = document.getElementById("expenditure-value");
+const balanceValue = document.getElementById("balance-amount");
+const list = document.getElementById("list");
+let tempAmount = 0;
+
+//Set Budget Part
+totalAmountButton.addEventListener("click", () => {
+    tempAmount = totalAmount.value;
+    amount.innerHTML = tempAmount;
+    balanceValue.innerText = tempAmount - expenditureValue.innerText;
+    totalAmount.value = "";
+})
+
+//Disable Edit and Delete Button
+const disableButtons = (bool) => {
+    let editButtons = document.getElementsByClassName("edit");
+    Array.from(editButtons).forEach((element) => {
+        element.disabled = bool;
+    });
+}
+
+//Modify List Elements
+const modifyElement = (element, edit = false) => {
+    let parentDiv = element.parentElement;
+    let currentBalance = balanceValue.innerText;
+    let currentExpense = expenditureValue.innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
+    
+    if (edit) {
+        let parentText = parentDiv.querySelector(".product").innerText;
+        productTitle.value = parentText;
+        userAmount.value = parentAmount; 
+        disableButtons(true);
+    }
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+    expenditureValue.innerText = parseInt(currentExpense) + parseInt(parentAmount);
+    parentDiv.remove();
+
+};
+
+//Create List
+const listCreator = (expenseName, expenseValue, category) => {
+    let sublistContent = document.createElement("div");
+    sublistContent.classList.add("sublist-content", "flex-space");
+
+    list.appendChild(sublistContent);
+    sublistContent.innerHTML = `<p class = "product">${expenseName}</p><p class = "amount">${expenseValue}</p>`;
+
+    let editButton = document.createElement("button");
+    editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
+    editButton.style.fontSize = "24px";
+    editButton.addEventListener("click", () => {
+        modifyElement(editButton, true);
+    });
+
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("fa-solid", "fa-trash-can", "delete");
+    deleteButton.style.fontSize = "24px";
+    deleteButton.addEventListener("click", () => {
+        modifyElement(deleteButton);
+    });
+
+    sublistContent.appendChild(editButton);
+    sublistContent.appendChild(deleteButton);
+    document.getElementById("list").appendChild(sublistContent);
+};
+
+//Add Expenses 
+checkAmountButton.addEventListener("click", () => {
+    disableButtons(false);
+
+    let expenditure = parseInt(userAmount.value);
+    let sum = parseInt(expenditureValue.innerText) + expenditure;
+    expenditureValue.innerText = sum;
+
+    const totalBalance = tempAmount - sum;
+    balanceValue.innerText = totalBalance;
+
+    listCreator(productTitle.value, userAmount.value);
+
+    productTitle.value = "";
+    userAmount.value = "";
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    }
+    }
+}
+}
+
+//Add Category 
+checkCategory.addEventListener("click", () => {
+    disableButtons(false);
+
+    let category = parseInt(userCategory.value);
+    listCreator(productTitle.value, userCategory.value);
+
+    productTitle.value = "";
+    userCategory.value = "";
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
